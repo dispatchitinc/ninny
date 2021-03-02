8d99a928069577aa93c8998b11f5a9047a93ed4e
Emma Sax <emma.sax4+git@gmail.com>
2021-03-01 18:02:10 -0600

    Adding some tests

--- spec/unit/setup_spec.rb
+++ spec/unit/setup_spec.rb
@@ -10,10 +10,18 @@ RSpec.describe Ninny::Commands::Setup do
     expect(subject).to receive(:try_reading_user_config)
     expect(subject).to receive(:prompt_for_gitlab_private_token)
     output = StringIO.new
-
     subject.execute(output: output)
-
     expect(output.string).to eq("User config !\n")
+  end
+
+  context 'when unable to write the config file via TTY' do
+    it 'should move on and update it anyway' do
+      allow(subject).to receive(:try_reading_user_config)
+      allow(subject).to receive(:prompt_for_gitlab_private_token).and_return('yyy')
+      allow(Ninny.user_config).to receive(:write).with(force: true).and_raise(StandardError)
+      expect(File).to receive(:open).and_return(true)
+      subject.execute(output: StringIO.new)
+    end
   end
 
   context '#try_reading_user_config' do
@@ -52,6 +60,30 @@ RSpec.describe Ninny::Commands::Setup do
       ).and_return('yyy')
       expect(Ninny.user_config).to receive(:set).with(:gitlab_private_token, value: 'yyy')
       subject.prompt_for_gitlab_private_token
+    end
+
+    context 'when unable to set via TTY' do
+      it 'should move on anyway' do
+        allow(Ninny.user_config).to receive(:gitlab_private_token)
+        allow_any_instance_of(TTY::Prompt).to receive(:yes?).with('Do you have a GitLab private token?').and_return(true)
+        allow_any_instance_of(TTY::Prompt).to receive(:ask).with(
+          'Enter private token:',
+          required: true
+        ).and_return('yyy')
+        allow(Ninny.user_config).to receive(:set).with(:gitlab_private_token, value: 'yyy').and_raise(ArgumentError)
+        subject.prompt_for_gitlab_private_token
+      end
+
+      it 'should return the private token' do
+        allow(Ninny.user_config).to receive(:gitlab_private_token)
+        allow_any_instance_of(TTY::Prompt).to receive(:yes?).with('Do you have a GitLab private token?').and_return(true)
+        allow_any_instance_of(TTY::Prompt).to receive(:ask).with(
+          'Enter private token:',
+          required: true
+        ).and_return('yyy')
+        allow(Ninny.user_config).to receive(:set).with(:gitlab_private_token, value: 'yyy').and_raise(ArgumentError)
+        expect(subject.prompt_for_gitlab_private_token).to eq('yyy')
+      end
     end
   end
 end
